b9e2e70226bd1b74524e3034742c89873b30aa59
Maikel ten Voorde <info@matevo.nl>
2019-02-18 10:42:47 +0100

    Initial Commit

--- src/Service.php
+++ src/Service.php
@@ -27,6 +27,8 @@ class Service extends Base
         switch ($package->offsetGet('execute')) {
             case "pair":
                 return $this->pair($package);
+            case "call":
+                return $this->call($package);
         }
     }
 
@@ -41,25 +43,75 @@ class Service extends Base
             throw new \Exception('Address property is missing!', 415);
         }
 
-        $address = $package->offsetGet('address');
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, "http://" . $address . "/api");
-        curl_setopt($ch, CURLOPT_POST, 1);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, 'devicetype=signalize');
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        $result = json_decode(curl_exec($ch));
-        curl_close($ch);
+        # Save address to config
+        Cache::set('module-hue', 'address', $package->offsetGet('address'));
+        $result = $this->callAPI(null, ['devicetype' => 'signalize']);
+        Cache::set('module-hue', 'username', $result[0]->success->username);
 
-        if ($result->error) {
-            throw new \Exception($result->error, 415);
+        return new Package(Cache::open('module-hue'));
+    }
+
+
+    /**
+     * @param Package $package
+     * @return Package
+     * @throws \Exception
+     */
+    private function call(Package $package)
+    {
+        if (!$package->offsetGet('endpoint')) {
+            throw new \Exception('Cannot execute! No endpoint defined!', 415);
         }
 
+        $credential = Cache::open('module-hue');
+        if (!$credential->address) {
+            throw new \Exception('Cannot execute! No paired address!', 415);
+        }
 
-        $data = Cache::get('module-hue');
-        $data->username = $result;
-        Cache::save('module-hue', $data);
+        $endpoint = $package->offsetGet('endpoint');
+        $data = $package->offsetGet('data');
 
-        return new Package($data);
+        $result = $this->callAPI($endpoint, $data);
+        return new Package($result);
+    }
+
+
+    /**
+     * @param null $uri
+     * @param null $data
+     * @return bool|string
+     * @throws \Exception
+     */
+    private function callAPI($uri = null, $data = null)
+    {
+        $settings = Cache::open('module-hue');
+        if (!$settings->address) {
+            throw new \Exception('Cannot execute! No saved settings found!', 415);
+        }
+
+        if ($uri !== null) {
+            if (!$settings->username) {
+                throw new \Exception('Cannot execute! Not paired to device!', 415);
+            }
+            $uri = "/" . $settings->username . "/" . $uri;
+        }
+
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, "http://" . $settings->address . "/api" . $uri);
+        if ($data !== null) {
+            curl_setopt($ch, CURLOPT_CUSTOMREQUEST, is_null($uri) ? "POST" : "PUT");
+            curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
+        }
+
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        $result = curl_exec($ch);
+        $json = json_decode($result);
+        curl_close($ch);
+
+        if (isset($result[0]) && $result[0]->error) {
+            throw new \Exception(json_encode($result[0]->error), 415);
+        }
+        return $json;
     }
 
 }
